8e2f266976ea5556884384f9305a6132fee2a704
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-05-07 19:02:50 -0600

    first

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBLwofrJRGfMVMLRA8N6hTb0HzGgYLo9NI",
+    authDomain: "train-app-3d466.firebaseapp.com",
+    databaseURL: "https://train-app-3d466.firebaseio.com",
+    projectId: "train-app-3d466",
+    storageBucket: "train-app-3d466.appspot.com",
+    messagingSenderId: "859374666106"
+};
+
+firebase.initializeApp(config);
